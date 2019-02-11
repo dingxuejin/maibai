80faa263b9c30b027a5fc9bbfdd6b1c8f1832a75
dingxuejin <761110187@qq.com>
2019-02-11 11:47:40 +0800

    11

--- utils/http.js
+++ utils/http.js
@@ -16,7 +16,7 @@ http.phoneNumber = '4006701808';
 // 充值说明Url
 http.chongzhiUrl ='http://h5.myobye.com:8088/index.html#index?question=3';
 // 用户手册Url
-http.userUrl ='http://h5.myobye.com:8088/index.html#index?question=12';
+http.userUrl ='http://h5.myobye.com:8088/index.html#index?question=12'   ;
 http.post = function (method, data = null) {
   return new Promise((resolve, reject) => {
     wx.request({
